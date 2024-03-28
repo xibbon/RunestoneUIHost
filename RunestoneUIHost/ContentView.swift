ec88c59d5bf551864b36edbfc5944e0be8c07bc0
Miguel de Icaza <miguel@gnome.org>
2024-03-28 15:01:30 -0400

    Initial Commit

---
+++ RunestoneUIHost/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunestoneUIHost
+//
+//  Created by Miguel de Icaza on 3/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunestoneUIHost/RunestoneUIHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunestoneUIHostApp.swift
+//  RunestoneUIHost
+//
+//  Created by Miguel de Icaza on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RunestoneUIHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
